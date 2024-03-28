7a3ddbc08eeed9052e7279a68e5c817e5a0f94ee
HTJoker <hersh.thakur@gmail.com>
2024-03-28 11:26:18 +0000

    created login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/my_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -16,8 +17,18 @@ class LoginPage extends StatelessWidget {
               size: 60,
               color: Theme.of(context).colorScheme.primary,
             ),
-
-            SizedBox()
+            const SizedBox(height: 50),
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 25),
+            const MyTextField(hintText: "Email"),
+            const SizedBox(height: 10),
+            const MyTextField(hintText: "Password", isObsure: true,)
           ],
         ),
       ),
